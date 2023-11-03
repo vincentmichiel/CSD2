ff5d512c69209f65ae99bb8125a425a67e23805e
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2023-11-03 22:13:00 +0100

    finished basic pointer interaction assigment

--- CSD2b/opdrachten/pointers/pointers/main.cpp
+++ CSD2b/opdrachten/pointers/pointers/main.cpp
@@ -1,86 +1,86 @@
 #include <iostream>
 
 /*
-  Assignment: complete this program by putting your code in all the locations
-    that say -- your code here --
-
-  Make sure that the program compiles and builds and check the output
-    after every change.
-
-  Tip: before you look at the output, always think ahead what you expect to
-    see and then check if your assumption was correct.
+ Assignment: complete this program by putting your code in all the locations
+ that say -- your code here --
+ 
+ Make sure that the program compiles and builds and check the output
+ after every change.
+ 
+ Tip: before you look at the output, always think ahead what you expect to
+ see and then check if your assumption was correct.
  */
 
 int main()
 {
-char letter = 97; // find this in the ASCII table (type 'man ascii')
-char *letterpointer;
-
-  std::cout << "Contents of the variable letter: ";
-  // -- your code here --
-
-  letterpointer = &letter;
-  std::cout << "Contents of letterpointer: ";
-  // -- your code here --
-
-  std::cout << "Contents of what letterpointer points to: ";
-  // -- your code here --
-
-  *letterpointer = 'b';
-  std::cout << "The variabele letter has gotten a new value through letterpointer.";
-  std::cout << "Contents of what letterpointer points to: ";
-  // -- your code here --
-
-  std::cout << "Contents of the variable letter: ";
-  // -- your code here --
-
-
-  /*
-   * Create the necessary pointer(s) and use them
-   */
-
-  unsigned short year = 2019;
-  std::cout << "Contents of the variabele year: ";
-  // -- your code here --
-  
-  // create a pointer to year
-  // -- your code here --
-
-  std::cout << "Contents of yearpointer: ";
-  // -- your code here --
-
-  std::cout << "Contents of what yearpointer points to: ";
-
-  // give year a new value via yearpointer
-  // -- your code here --
-
-  std::cout << "Contents of the variabele year: ";
-  // -- your code here --
-
-
-  // create another pointer to year, named anotheryearpointer
-  // -- your code here --
-
-  std::cout << "Contents of anotheryearpointer: ";
-  // -- your code here --
-
-  std::cout << "Contents of what anotheryearpointer points to: ";
-  // -- your code here --
-
-  // give year a new value via anotheryearpointer
-  // -- your code here --
-
-  std::cout << "Contents of year: ";
-  // -- your code here --
-
-  std::cout << "Contents of what anotheryearpointer points to: ";
-  // -- your code here --
-
-  //anotheryearpointer++;
-
-  std::cout << "Contents of anotheryearpointer after ++: ";
-  // -- your code here --
-
-
+    char letter = 97; // find this in the ASCII table (type 'man ascii')
+    char *letterpointer;
+    
+    std::cout << "Contents of the variable letter: ";
+    std::cout << letter << std::endl;
+    
+    letterpointer = &letter;
+    std::cout << "Contents of letterpointer: ";
+    std::cout << letterpointer << std::endl;
+    
+    std::cout << "Contents of what letterpointer points to: ";
+    std::cout << *letterpointer << std::endl;
+    
+    *letterpointer = 'b';
+    std::cout << "The variabele letter has gotten a new value through letterpointer." << std::endl;
+    std::cout << "Contents of what letterpointer points to: ";
+    std::cout << *letterpointer << std::endl;
+    
+    std::cout << "Contents of the variable letter: ";
+    std::cout << letter << std::endl;
+    
+    
+    /*
+     * Create the necessary pointer(s) and use them
+     */
+    
+    unsigned short year = 2019;
+    std::cout << "Contents of the variabele year: ";
+    std::cout << year << std::endl;
+    
+    // create a pointer to year
+    unsigned short *yearPtr = &year;
+    
+    std::cout << "Contents of yearpointer: ";
+    std::cout << yearPtr << std::endl;
+    
+    std::cout << "Contents of what yearpointer points to: ";
+    std::cout << *yearPtr << std::endl;
+    // give year a new value via yearpointer
+    *yearPtr = 2023;
+    
+    std::cout << "Contents of the variabele year: ";
+    std::cout << year << std::endl;
+    
+    
+    // create another pointer to year, named anotheryearpointer
+    unsigned short *anotheryearpointer = &year;
+    
+    std::cout << "Contents of anotheryearpointer: ";
+    std::cout << anotheryearpointer << std::endl;
+    
+    std::cout << "Contents of what anotheryearpointer points to: ";
+    std::cout << *anotheryearpointer << std::endl;
+    
+    // give year a new value via anotheryearpointer
+    *anotheryearpointer = 2001;
+    
+    std::cout << "Contents of year: ";
+    std::cout << year << std::endl;
+    
+    std::cout << "Contents of what anotheryearpointer points to: ";
+    std::cout << *anotheryearpointer << std::endl;
+    
+    anotheryearpointer++;
+    
+    std::cout << "Contents of anotheryearpointer after ++: ";
+    std::cout << *anotheryearpointer << std::endl;
+    
+    
 } // main()
 
