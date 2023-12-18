df2b0b8a03d9d8fb447d8445b2880a2f222ed949
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2023-12-18 13:22:13 +0100

    implemented ui test

--- CSD2b/eindopdracht/src/SynthSong_JACK_rewrite/SynthSong_JACK_rewrite/main.cpp
+++ CSD2b/eindopdracht/src/SynthSong_JACK_rewrite/SynthSong_JACK_rewrite/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include "CustomCallback.h"
+#include "UI.hpp"
 
 
 int main() {
     CustomCallback callback = CustomCallback {};
     JackModule jackModule = JackModule { callback };
     jackModule.init (0, 1);
+    
+    UI ui;
+    std::string waveFormOptions[4] = {"sine", "saw", "square", "triangle"};
+    
+    int waveFormSelection = ui.retrieveUserSelection(waveFormOptions, 4);
+    
+    std::cout << waveFormSelection << std::endl;
     
     // clear plot file
     std::ofstream ofs;
