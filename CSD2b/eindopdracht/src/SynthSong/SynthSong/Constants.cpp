be1cbc184408b8671cf9afb40edcaf08b068f8f3
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-02-14 15:47:28 +0100

    gitignore fix

--- CSD2b/eindopdracht/src/SynthSong/SynthSong/Constants.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Constants.cpp
-//  SynthSong
-//
-//  Created by Vincent Van den Broeck on 07/11/2023.
-//
-
-#include "Constants.hpp"
--- README.md
+++
@@ -1,1 +0,0 @@
-Repository for CSD2 assignments and projects
