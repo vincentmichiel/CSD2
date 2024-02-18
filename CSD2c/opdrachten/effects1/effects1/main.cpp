590710539e4c67852e243999c0ceb0580bae9fdd
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-02-18 16:44:30 +0100

    wavetable generator example

--- CSD2c/opdrachten/effects1/effects1/main.cpp
+++ CSD2c/opdrachten/effects1/effects1/main.cpp
@@ -6,12 +6,38 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "CustomCallback.h"
+#include "WavetableGenerator.hpp"
+
+#define BYPASS_AUDIO_LOOP 0
+
+#define BUFFER_SIZE 1024
 
 int main(int argc, const char * argv[]) {
     // init audio
     CustomCallback callback = CustomCallback {};
     JackModule jackModule = JackModule { callback };
+    
+#if BYPASS_AUDIO_LOOP
+    // generate wavetable and fill with sine waveform
+    float wavetable[BUFFER_SIZE];
+    WavetableGenerator::generateWaveform(wavetable, BUFFER_SIZE,
+                                         WavetableGenerator::WaveformType::SINE);
+    
+    // clear plot file
+    std::ofstream ofs;
+    ofs.open("waveform.txt", std::ofstream::out | std::ofstream::trunc);
+    
+    // write to file
+    for(int i = 0; i < BUFFER_SIZE; i++){
+        ofs << i << ":" << wavetable[i] << std::endl;
+    }
+    
+    // close file
+    ofs.close();
+
+#else
     jackModule.init(1, 1);
     
     bool running = true;
@@ -21,6 +47,7 @@ int main(int argc, const char * argv[]) {
                 running = false;
         }
     }
+#endif
     
     return 0;
 }
