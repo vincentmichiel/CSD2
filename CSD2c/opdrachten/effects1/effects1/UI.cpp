47ecfa21bbbac76c3f16150d280c17a6c1e269a8
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-02-19 23:13:26 +0100

    included ui

---
+++ CSD2c/opdrachten/effects1/effects1/UI.cpp
@@ -0,0 +1,8 @@
+//
+//  UI.cpp
+//  effects1
+//
+//  Created by Vincent Van den Broeck on 19/02/2024.
+//
+
+#include "UI.hpp"
---
+++ CSD2c/opdrachten/effects1/effects1/UI.hpp
@@ -0,0 +1,13 @@
+//
+//  UI.hpp
+//  effects1
+//
+//  Created by Vincent Van den Broeck on 19/02/2024.
+//
+
+#ifndef UI_hpp
+#define UI_hpp
+
+#include <stdio.h>
+
+#endif /* UI_hpp */
