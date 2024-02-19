b805106c0b5a24fbb585e6fd0ab0cbe14fbdf599
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-02-19 23:15:23 +0100

    added ui input to set effects

--- CSD2c/opdrachten/effects1/effects1/main.cpp
+++ CSD2c/opdrachten/effects1/effects1/main.cpp
@@ -8,36 +8,51 @@
 #include <iostream>
 #include <fstream>
 #include "CustomCallback.h"
-#include "WavetableGenerator.hpp"
+#include "UI.hpp"
 
 #define BYPASS_AUDIO_LOOP 0
 
-#define BUFFER_SIZE 1024
-
 int main(int argc, const char * argv[]) {
+    UI ui;
     // init audio
     CustomCallback callback = CustomCallback {};
     JackModule jackModule = JackModule { callback };
     
 #if BYPASS_AUDIO_LOOP
-    // generate wavetable and fill with sine waveform
-    float wavetable[BUFFER_SIZE];
-    WavetableGenerator::generateWaveform(wavetable, BUFFER_SIZE,
-                                         WavetableGenerator::WaveformType::SINE);
-    
-    // clear plot file
-    std::ofstream ofs;
-    ofs.open("waveform.txt", std::ofstream::out | std::ofstream::trunc);
-    
-    // write to file
-    for(int i = 0; i < BUFFER_SIZE; i++){
-        ofs << i << ":" << wavetable[i] << std::endl;
-    }
-    
-    // close file
-    ofs.close();
+   
 
 #else
+    // user input
+    bool enableTremolo = true, enableDelay = true, enableWaveshaper = true;
+    float tremoloMix = 0.0f, delayMix = 0.0f, waveshaperMix = 0.0f;
+    float tremoloFrequency = 1.0f;
+    float tremoloDepth = 0.0f;
+    float delaySeconds = 0.0f;
+    float delayFeedback = 0.0f;
+    float waveshaperDrive = 0.0f;
+    
+    enableTremolo = ui.getYesNo("Enable tremolo?");
+    if(enableTremolo){
+        tremoloMix = ui.getFloat("Set the tremolo mix: ", 0.0f, 1.0f);
+        tremoloFrequency = ui.getFloat("Set the tremolo frequency: ", 0.1f, 100.0f);
+        tremoloDepth = ui.getFloat("Set the tremolo depth: ", 0.0f, 1.0f);
+    }
+    
+    enableDelay = ui.getYesNo("Enable delay?");
+    if(enableDelay){
+        delayMix = ui.getFloat("Set the delay mix: ", 0.0f, 1.0f);
+        delaySeconds = ui.getFloat("How many seconds delay?", 0.1, 10.0f);
+        delayFeedback = ui.getFloat("Set delay feedback: ", 0.0f, 0.99f);
+    }
+    
+    enableWaveshaper = ui.getYesNo("Enable waveshaper?");
+    if(enableWaveshaper){
+        waveshaperMix = ui.getFloat("Set the waveshaper mix: ", 0.0f, 1.0f);
+        waveshaperDrive = ui.getFloat("Set the waveshaper drive: ", 0.0f, 50.0f);
+    }
+    
+    callback.setEffects(enableTremolo, enableDelay, enableWaveshaper, tremoloMix, delayMix, waveshaperMix, tremoloFrequency, tremoloDepth, delaySeconds, delayFeedback, waveshaperDrive);
+    
     jackModule.init(1, 1);
     
     bool running = true;
