4ae8e48b1ebcfff343c6c23b5474b284482086b8
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-01 16:24:31 +0100

    filters from cc4

---
+++ CSD2c/opdrachten/effects1/effects1/audiocomponent.h
@@ -0,0 +1,12 @@
+//
+//  audiocomponent.h
+//  effects1
+//
+//  Created by Vincent Van den Broeck on 01/03/2024.
+//
+
+#ifndef audiocomponent_h
+#define audiocomponent_h
+
+
+#endif /* audiocomponent_h */
---
+++ CSD2c/opdrachten/effects1/effects1/deltasequence.h
@@ -0,0 +1,12 @@
+//
+//  deltasequence.h
+//  effects1
+//
+//  Created by Vincent Van den Broeck on 01/03/2024.
+//
+
+#ifndef deltasequence_h
+#define deltasequence_h
+
+
+#endif /* deltasequence_h */
