6fae9e0d2b49c399f29205a07a96835b1cfa3959
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-01 16:25:12 +0100

    plotting tool

--- CSD2c/opdrachten/effects1/effects1/deltasequence.h
+++ CSD2c/opdrachten/effects1/effects1/deltasequence.h
@@ -1,12 +1,18 @@
 //
-//  deltasequence.h
-//  effects1
-//
-//  Created by Vincent Van den Broeck on 01/03/2024.
+// Created by Dean on 28/02/2024.
 //
 
-#ifndef deltasequence_h
-#define deltasequence_h
+// Complex Wissel Signaal
+
+#include <iostream>
+#include "Filters.h"
 
 
-#endif /* deltasequence_h */
+struct DeltaSequence {
+    static void run(Filter& filter){
+        for (auto i = -3; i < 14; ++i){
+            std::cout << i << ": " << filter.process( i == 0 ? 1 : 0  ) << std::endl;
+        }
+    }
+};
+
