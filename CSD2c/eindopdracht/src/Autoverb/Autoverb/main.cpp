860f9b2db09bdc52b4a5860b0dfa10f16a9c8c57
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-22 17:57:55 +0100

    updated file structure

---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 22/03/2024.
+//
+
+#include <iostream>
+#include "CustomCallback.h"
+
+int main(int argc, const char * argv[]) {
+    // init audio
+    CustomCallback callback = CustomCallback {};
+    JackModule jackModule = JackModule { callback };
+    
+    jackModule.init(1, 2);
+    
+    bool running = true;
+    while (running) {
+        switch (std::cin.get()) {
+            case 'q':
+                running = false;
+        }
+    }
+    return 0;
+}
