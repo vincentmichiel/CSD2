1f5373ad6bc43741373095ac30fa32f42a3ed247
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-22 18:08:24 +0100

    added delay class

---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/Delay.cpp
@@ -0,0 +1,43 @@
+//
+//  Delay.cpp
+//  effects1
+//
+//  Created by Vincent Van den Broeck on 08/02/2024.
+//
+
+#include "Delay.hpp"
+
+Delay::~Delay(){
+    Effect::~Effect();
+};
+
+void Delay::setFeedback(float feedback){
+    this->feedback = feedback;
+}
+
+uint Delay::getSize(){
+    return size;
+}
+
+uint Delay::getDistance(){
+    return distance;
+}
+
+float Delay::getFeedback(){
+    return feedback;
+}
+
+void Delay::tick(){
+    circBuffer.tick();
+}
+
+float Delay::applyEffect(int channel, float sample){
+    // read sample from delayline
+    float delayedSample = circBuffer.read();
+    
+    // delay + feedback
+    circBuffer.write(sample + delayedSample * feedback);
+    
+    // output
+    return delayedSample;
+}
