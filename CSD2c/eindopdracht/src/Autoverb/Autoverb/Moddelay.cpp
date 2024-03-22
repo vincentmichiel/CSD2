a64c2469af3b7f7508eee4fcf70d40349849a2fa
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-22 21:32:17 +0100

    added moddelay files

---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/Moddelay.cpp
@@ -0,0 +1,8 @@
+//
+//  Moddelay.cpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 22/03/2024.
+//
+
+#include "Moddelay.hpp"
---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/Moddelay.hpp
@@ -0,0 +1,13 @@
+//
+//  Moddelay.hpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 22/03/2024.
+//
+
+#ifndef Moddelay_hpp
+#define Moddelay_hpp
+
+#include <stdio.h>
+
+#endif /* Moddelay_hpp */
