8a2d22cfe7b072fad6d695322bc92a3408e54e8d
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-22 18:29:00 +0100

    added multitapdelay files

---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/MultitapDelay.cpp
@@ -0,0 +1,8 @@
+//
+//  MultitapDelay.cpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 22/03/2024.
+//
+
+#include "MultitapDelay.hpp"
---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/MultitapDelay.hpp
@@ -0,0 +1,13 @@
+//
+//  MultitapDelay.hpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 22/03/2024.
+//
+
+#ifndef MultitapDelay_hpp
+#define MultitapDelay_hpp
+
+#include <stdio.h>
+
+#endif /* MultitapDelay_hpp */
