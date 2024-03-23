5902585594f1f4b63cabbb4d7a801f69bcb36b41
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-03-23 16:07:10 +0100

    added reverb class files

---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/Reverb.cpp
@@ -0,0 +1,8 @@
+//
+//  Reverb.cpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 23/03/2024.
+//
+
+#include "Reverb.hpp"
---
+++ CSD2c/eindopdracht/src/Autoverb/Autoverb/Reverb.hpp
@@ -0,0 +1,13 @@
+//
+//  Reverb.hpp
+//  Autoverb
+//
+//  Created by Vincent Van den Broeck on 23/03/2024.
+//
+
+#ifndef Reverb_hpp
+#define Reverb_hpp
+
+#include <stdio.h>
+
+#endif /* Reverb_hpp */
