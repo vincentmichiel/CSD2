2afaa31dee3f621f8e720a855515192678d13992
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-05-10 23:08:48 +0200

    updated gitignore to ignore audio plugin export files

---
+++ CSD2d/src/FFTDemo/Builds/MacOSX/Oscillator.cpp
@@ -0,0 +1,8 @@
+//
+//  Oscillator.cpp
+//  FFTDemo - All
+//
+//  Created by Vincent Van den Broeck on 10/05/2024.
+//
+
+#include "Oscillator.hpp"
---
+++ CSD2d/src/FFTDemo/Builds/MacOSX/Oscillator.hpp
@@ -0,0 +1,13 @@
+//
+//  Oscillator.hpp
+//  FFTDemo - All
+//
+//  Created by Vincent Van den Broeck on 10/05/2024.
+//
+
+#ifndef Oscillator_hpp
+#define Oscillator_hpp
+
+#include <stdio.h>
+
+#endif /* Oscillator_hpp */
