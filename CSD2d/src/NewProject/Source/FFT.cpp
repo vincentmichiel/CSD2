af4d7f72d6b388fb7a3baaeca6686c8eadf4c9ce
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-05-12 15:34:16 +0200

    added FFT wrapper

--- CSD2d/src/NewProject/Source/FFT.cpp
+++ CSD2d/src/NewProject/Source/FFT.cpp
@@ -1,11 +1,89 @@
 /*
-  ==============================================================================
-
-    FFT.cpp
-    Created: 11 May 2024 1:16:32pm
-    Author:  Vincent Van den Broeck
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ FFT.cpp
+ Created: 11 May 2024 1:16:32pm
+ Author:  Vincent Van den Broeck
+ 
+ ==============================================================================
+ */
 
 #include "FFT.h"
+
+FFT::FFT() : fft(fftOrder), window(fftSize + 1, juce::dsp::WindowingFunction<float>::WindowingMethod::hann, false) {
+    count = 0;
+    pos = 0;
+}
+
+FFT::~FFT(){};
+
+int FFT::getLatencyInSamples() const {
+    return pow(2, fftOrder);
+}
+
+void FFT::reset(){
+    count = 0;
+    pos = 0;
+    std::fill(inputFifo.begin(), inputFifo.end(), 0.0f);
+    std::fill(outputFifo.begin(), outputFifo.end(), 0.0f);
+}
+
+float FFT::processSample(float sample, bool bypassed) {
+    inputFifo[pos] = sample;
+    
+    // store output sample
+    float outputSample = outputFifo[pos];
+    outputFifo[pos] = 0.0f;
+    
+    pos += 1;
+    // constrain pos
+    if (pos == fftSize) {
+        pos = 0;
+    }
+    
+    // constrain pos and apply fft when buffer is filled
+    count += 1;
+    if (count == hopSize) {
+        count = 0;
+        processFrame(bypassed);
+    }
+    
+    return outputSample;
+}
+
+void FFT::processFrame(bool bypassed) {
+    const float* inputPtr = inputFifo.data();
+    float* fftPtr = fftData.data();
+    
+    std::memcpy(fftPtr, inputPtr + pos, (fftSize - pos) * sizeof(float));
+    if (pos > 0) {
+        std::memcpy(fftPtr + fftSize - pos, inputPtr, pos * sizeof(float));
+    }
+    
+    // apply windowing
+    window.multiplyWithWindowingTable(fftPtr, fftSize);
+    
+    // apply fft
+    if (!bypassed) {
+        fft.performRealOnlyForwardTransform(fftPtr, true);  // FFT
+        processSpectrum(fftPtr, numBins);
+        fft.performRealOnlyInverseTransform(fftPtr);        // IFFT
+    }
+    
+    // apply output windowing
+    window.multiplyWithWindowingTable(fftPtr, fftSize);
+    
+    for (int i = 0; i < fftSize; ++i) {
+        fftPtr[i] *= windowCorrection;
+    }
+    
+    // write to output buffer
+    for (int i = 0; i < pos; ++i) {
+        outputFifo[i] += fftData[i + fftSize - pos];
+    }
+    for (int i = 0; i < fftSize - pos; ++i) {
+        outputFifo[i + pos] += fftData[i];
+    }
+}
+
+void FFT::processSpectrum(float* data, int numBins){};
--- CSD2d/src/NewProject/Source/FFT.h
+++ CSD2d/src/NewProject/Source/FFT.h
@@ -1,11 +1,47 @@
 /*
-  ==============================================================================
-
-    FFT.h
-    Created: 11 May 2024 1:16:32pm
-    Author:  Vincent Van den Broeck
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ FFT.h
+ Created: 11 May 2024 1:16:32pm
+ Author:  Vincent Van den Broeck
+ 
+ FFT Wrapper based on https://audiodev.blog/fft-processing/
+ 
+ ==============================================================================
+ */
 
 #pragma once
+#include <JuceHeader.h>
+
+class FFT {
+public:
+    FFT();
+    virtual ~FFT();
+    int getLatencyInSamples() const;
+    void reset();
+    float processSample(float sample, bool bypassed);
+    void processBlock(float* data, int numSamples, bool bypassed);
+    
+    static constexpr int fftOrder = 10;
+    static constexpr int fftSize = 1 << fftOrder;      // left shift operator << 1024 samples
+    
+protected:
+    virtual void processSpectrum(float* data, int numBins);
+
+private:
+    void processFrame(bool bypassed);
+    
+    juce::dsp::FFT fft;
+    juce::dsp::WindowingFunction<float> window;
+    static constexpr float windowCorrection = 2.0f / 3.0f;
+    
+    int count;
+    int pos;
+    std::array<float, fftSize> inputFifo;
+    std::array<float, fftSize> outputFifo;
+    std::array<float, fftSize * 2> fftData;
+    
+    static constexpr int numBins = fftSize / 2 + 1;    // 513 bins (up to niquist)
+    static constexpr int overlap = 4;                  // 75% overlap
+    static constexpr int hopSize = fftSize / overlap;  // 256 samples
+};
