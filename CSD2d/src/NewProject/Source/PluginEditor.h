c1f41b3bff68c208c74ddc088d4c16735a16c75b
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-05-18 17:06:07 +0200

    added ReEsser GUI controls

--- CSD2d/src/NewProject/Source/PluginEditor.h
+++ CSD2d/src/NewProject/Source/PluginEditor.h
@@ -14,7 +14,7 @@
 //==============================================================================
 /**
 */
-class NewProjectAudioProcessorEditor  : public juce::AudioProcessorEditor
+class NewProjectAudioProcessorEditor  : public juce::AudioProcessorEditor, private juce::Slider::Listener
 {
 public:
     NewProjectAudioProcessorEditor (NewProjectAudioProcessor&);
@@ -28,6 +28,11 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     NewProjectAudioProcessor& audioProcessor;
+    juce::Slider threshHold;
+    juce::Slider bandWidth;
+    juce::Slider centerFrequency;
+    
+    void sliderValueChanged(juce::Slider* slider) override;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
 };
