b464c6f38749547a9d4f6a796fa1119fa7cc35e3
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-06-01 15:38:37 +0200

    added resonator class

---
+++ CSD2d/src/SoundScaper/Source/Resonator.cpp
@@ -0,0 +1,52 @@
+/*
+  ==============================================================================
+
+    Squarer.cpp
+    Created: 21 May 2024 8:58:02am
+    Author:  Vincent Van den Broeck
+
+  ==============================================================================
+*/
+
+#include "Resonator.h"
+#include "Oscillator.hpp"
+#include "Interpolate.h"
+
+Resonator::~Resonator(){};
+
+void Resonator::prepare(float frequency, float depth, double samplerate){
+    lfo = new Sine(frequency, depth, samplerate);
+}
+
+void Resonator::setDepth(float depth){
+    this->depth = depth;
+}
+
+void Resonator::processSpectrum(float* data)
+{
+    auto* cdata = reinterpret_cast<std::complex<float>*>(data);
+    float normalisedLevelValues[numBins];
+    
+    for(int i = 0; i < numBins; i++){
+        normalisedLevelValues[i] = std::abs(spectrumAnalyser.sidechainFFT[i]);
+    }
+    
+    juce::Range<float> levelsMinMax = juce::FloatVectorOperations::findMinAndMax(normalisedLevelValues, numBins);
+    
+    
+    // spectral processing
+    
+    for (int i = 0; i < numBins; ++i) {
+        float magnitude = std::abs(cdata[i]);
+        float phase = std::arg(cdata[i]);
+        
+        normalisedLevelValues[i] = Interpolate::mapInRange(normalisedLevelValues[i], (float) levelsMinMax.getStart(), (float) levelsMinMax.getEnd(), 0, 1);
+        
+        magnitude *= (1 - depth);
+        magnitude += normalisedLevelValues[i] * depth;
+        
+        
+        // backwards conversion
+        cdata[i] = std::polar(magnitude, phase);
+    }
+}
